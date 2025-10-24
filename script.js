381eaaccc5b0d5998370178d314c1aab39d44b1a
Khoo Teng Jin <tengjinkhoo02@gmail.com>
2025-10-24 23:18:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.querySelectorAll('.project-card').forEach(card => {
+  card.addEventListener('click', () => {
+    alert(`You clicked on: ${card.querySelector('h3').textContent}`);
+  });
+});
